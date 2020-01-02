69e86e83d6699cb583fdb0e59a578f80d6cb67d0
muhao <806919059@qq.com>
2020-01-02 16:53:56 +0800

    hello world created

---
+++ build/CMakeFiles/hello_node.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello_node.dir/src/helloworld.cpp.o"
+  "hello_node.pdb"
+  "hello_node"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hello_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
